da511696b26efc1c5c4b8b8df8cb4c3b65bc5d65
Kannan14th08th <kannan14th08th@gmail.com>
2025-02-01 23:01:53 +0530

    Week1_Day1

---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version= 3.14f;
+		String webdriverTool="Selenium";
+		boolean isavailable=false;
+		int releaseYear=2017;
+		char shortcutKey='s';
+
+		System.out.println(version);
+		System.out.println(webdriverTool);
+		System.out.println(webdriverTool);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+
+	}
+
+}
